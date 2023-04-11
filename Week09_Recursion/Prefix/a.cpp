755577ed7da6e5534e47f5190e845e519cb13c50
Frank <frank840629@gmail.com>
2023-04-11 23:43:33 +0800

    update

--- Week09_Recursion/Prefix/a.cpp
+++ Week09_Recursion/Prefix/a.cpp
@@ -1,43 +1,38 @@
-// A piece of C code
 #include <stdio.h>
 #include <ctype.h>
-int cal(void){
-    int ans, op1, op2;
-    char c = getchar();
-    if ( c == ' ' ) return cal();
-    if ( isdigit(c) ) {
-        ungetc(c, stdin);
-        scanf("%d", &ans);
-        printf("%d", ans);
-    }
-    if( c == '+' ){
-      printf("(");
-      op1 = cal();
-      printf("+");
-      op2 = cal();
-      printf(")");
-      ans = op1 + op2;
-    }
-    if( c == '-' ){
-      printf("(");
-      op1 = cal();
-      printf("-");
-      op2 = cal();
-      printf(")");
-      ans = op1 - op2;
-    }
-    if( c == '*' ){
-      printf("(");
-      op1 = cal();
-      printf("*");
-      op2 = cal();
-      printf(")");
-      ans = op1 * op2;
-    }
+
+int cal(void)
+{
+  int ans, op1, op2;
+  char c = getchar();
+  if (c == ' ') {
+		return cal();
+	} else if (isdigit(c)) {
+    ungetc(c, stdin);
+    scanf("%d", &ans);
+    printf("%d", ans);
     return ans;
+  } 
+  
+  printf("(");
+  op1 = cal();
+  printf(" %c ", c);
+  op2 = cal();  
+  printf(")");
+    
+  if(c == '+') {
+		return op1 + op2;
+	} else if(c == '-') {
+		return op1 - op2;
+	} else if(c == '*') {
+		return op1 * op2;
+	}
 }
-int main(void){
-    // * + 2 3 + 4 + - 5 6 - 7 8
-    printf(" = %d\n", cal());
-    return 0;
+
+int main(void)
+{
+  printf(" = %d\n", cal());
+  return 0;
 }
+
+// ((2 + 3) * (4 + ((5 - 6) + (17 - 18))))
